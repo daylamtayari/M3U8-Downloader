13c41b71cb628e524684c9967736a41b7ce2c25d
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2021-01-13 01:47:52 -0700

    Completed the TSDownload method
    - Completed the TSDownload method in the Downloader class which processes the downloading of each individual TS file from a batch of TS links.
    - Changed the visibility identifier of the tempDownload method of the Downloader class from private to protected.

--- src/Downloader.java
+++ src/Downloader.java
@@ -20,6 +20,14 @@ import org.apache.commons.io.FilenameUtils;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.NavigableMap;
+import java.util.Queue;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * This class contains all of the downloading methods
@@ -36,7 +44,7 @@ public class Downloader {
      * @return File     File object equal to the file which was created and downloaded to.
      * @throws IOException
      */
-    private static File tempDownload(String url) throws IOException {
+    protected static File tempDownload(String url) throws IOException {
         URL dURL=new URL(url);
         String prefix=FilenameUtils.getBaseName(dURL.getPath());
         //Edit the prefix length:
@@ -64,5 +72,53 @@ public class Downloader {
         downloadFile.deleteOnExit();    //Delete the file when the program exits.
         FileUtils.copyURLToFile(dURL, downloadFile, TIMEOUT, TIMEOUT);      //Downloads the contents of the URL to the file we just made.
         return downloadFile;
+    }
+
+    /**
+     * This method processes the downloading of a series of
+     * TS file links, which are the file type of videos contained
+     * in M3U8 playlists.
+     * @param links     String arraylist which contains all of the TS links to download.
+     * @return NavigableMap<Integer, File>      Navigable map which contains an integer value, representing the order of each feed,
+     * and a file object which represents the corresponding file.
+     */
+    protected static NavigableMap<Integer, File> TSDownload(ArrayList<String> links){
+        NavigableMap<Integer, File> segmentMap=new TreeMap<>();
+        Queue<String> downloadQueue=new ConcurrentLinkedQueue<>();
+        for(String link: links){    //Add all of the links that are in the inputted links arraylist to the download queue.
+            downloadQueue.offer(link);
+        }
+        int index=0;
+        ThreadPoolExecutor downloadTPE=(ThreadPoolExecutor) Executors.newFixedThreadPool(THREAD_COUNT);
+        while(!downloadQueue.isEmpty()){
+            index++;
+            String item=downloadQueue.poll();
+            final int finalIndex=index;
+            downloadTPE.execute(new Runnable() {
+                int currentTries=1;
+                @Override
+                public void run() {
+                    while(currentTries<=MAX_TRIES){     //Keeps trying until download is successful or it has tried it more than the maximum amount of tries.
+                        final int threadIndex=finalIndex;
+                        final String threadItem=item;
+                        try{
+                            File tempTS=tempDownload(threadItem);
+                            segmentMap.put(threadIndex, tempTS);
+                            break;      //If the download and assignment to the navigable map successful, break the loop.
+                        }
+                        catch(IOException ignored){}
+                        //For demonstration purposes, any exceptions are simply ignored but you would want to implement proper error handling.
+                    }
+                }
+            });
+        }
+        downloadTPE.shutdown();
+        try{    //Terminate the thread pool executor and if it takes too long and times out, force interrupt it.
+            downloadTPE.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        }
+        catch(Exception e){
+            Thread.currentThread().interrupt();
+        }
+        return segmentMap;
     }
 }
