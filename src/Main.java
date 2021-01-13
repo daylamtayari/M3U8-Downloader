26c6d6b9b90ff4cefd2df144ae7761f7a6c89a88
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2021-01-12 23:59:00 -0700

    Main method prompts
    - Created the main class which contains the main method.
    - Completed the main method to its entirety, excluding the actual method call to the downloader.

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2020, 2021 Daylam Tayari <daylam@tayari.gg>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not see http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  @author Daylam Tayari daylam@tayari.gg https://github.com/daylamtayari
+ *  @version 1.0
+ *  Github project home page: https://github.com/daylamtayari/M3U8-Downloader
+ */
+
+import java.util.Scanner;
+
+/**
+ * This is the main class which
+ * contains the main method of
+ * the program.
+ */
+public class Main {
+    /**
+     * Main method of the program.
+     * @param args
+     */
+    public static void main(String[] args){
+        System.out.print(
+                  "\nM3U8 Downloader:"
+                + "\nThis is a proof of concept program for a M3U8 downloader using Java."
+                + "\nAuthor: Daylam Tayari https://github.com/daylamtayari https://paypal.me/daylamtayari"
+                + "\n"
+        );
+        Scanner sc=new Scanner(System.in);
+        System.out.print("\nM3U8 URL: ");
+        String url=sc.next();
+        System.out.print("\nOutput FOLDER path: ");
+        String folder=sc.next();
+        //This assumes that the folder path entered ended with a '\'.
+        //When implementing this into an actual program, add a '\' at the end if the user did not.
+        System.out.print("\nOutput file name: ");
+        String name=sc.next();
+        String fp=folder+name+".ts";
+        sc.close();
+        //TODO: Add downloader method call.
+        System.out.print(
+                  "\nDownload complete!"
+                + "\nFile downloaded at: "+fp
+        );
+    }
+}
